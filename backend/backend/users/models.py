d0f9d8c68ed113c75fac43d5ac2452a6c96643f3
Ray Lee <wooo519@naver.com>
2023-01-10 17:39:35 +0900

    fix : users test_2

--- backend/backend/users/models.py
+++ backend/backend/users/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Users(models.Model):
     id = models.AutoField(primary_key=True)
-    email = models.CharField(unique=True, max_length=200)
+    email = models.CharField(unique=True, max_length=200, null=True, blank=True)
     password = models.BinaryField(max_length=60)
     alias = models.CharField(unique=True, max_length=20)
     active = models.BooleanField(default=True)
