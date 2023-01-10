e89eb9b298a9c2c8a90f42389bea86f6edb2e621
devGEP <ju0032564@gmail.com>
2023-01-10 17:44:56 +0900

    feat : header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {Routes, Route} from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./pages/home/Home";
+import MyPage from "./pages/my-page/MyPage";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <React.Fragment>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/my-page" element={<MyPage />} />
+      </Routes>
+    </React.Fragment>
+  )
 }
 
 export default App;
