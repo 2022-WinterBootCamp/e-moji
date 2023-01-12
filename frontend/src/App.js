7dd041aeeb19b592cf0bc415b4886ad4f32282d6
devGEP <ju0032564@gmail.com>
2023-01-12 15:20:34 +0900

    fix : header
    mui 적용

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,12 @@
 import React from "react";
-import {Routes, Route} from "react-router-dom";
-import Header from "./components/Header";
-import Home from "./pages/home/Home";
-import MyPage from "./pages/my-page/MyPage";
+import Header from "./components/common/Header";
 
-const App = () => {
-  return (
-    <React.Fragment>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/my-page" element={<MyPage />} />
-      </Routes>
-    </React.Fragment>
-  )
+function App() {
+    return(
+        <React.Fragment>
+            <Header />
+        </React.Fragment>
+    );
 }
 
 export default App;
