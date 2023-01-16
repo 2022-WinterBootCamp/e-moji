5f638b722bb748ea993d134d1af2cc3d79514d29
Soyoon Park <spurt@tukorea.ac.kr>
2023-01-17 02:13:23 +0900

    feat : emojis upload setting
    admin, models, serializers, urls, views

--- backend/backend/emojis/admin.py
+++ backend/backend/emojis/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Emojis
 # Register your models here.
+
+@admin.register(Emojis)
+class EmogiAdmin(admin.ModelAdmin):
+    
+    list_display = ['id','user_id', 'kind', 'image'] # 'name'도 넣어주어야 하는지?
--- backend/backend/emojis/views.py
+++ backend/backend/emojis/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+import io
+from PIL import Image
 
+from django.http import JsonResponse
+
+from rest_framework import status, viewsets
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.decorators import api_view
+
+from .serializers import PictureSerializer # 클래스 이름 다르게 해야 하는지?
+from datetime import datetime, timedelta
+from .utils import get_img_url, create_img
+from users.models import Users
 # Create your views here.
+
+@api_view(['POST'])
+def emojis(request):
+# 빈 리스트를 만들어서 for문으로 0~6까지 kind에 넣어주는 방법이 괜찮은지?
+    return JsonResponse(data, status = 200)
