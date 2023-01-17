107da18e6cbde40f43e1a66debc448d4860e0421
Ray Lee <wooo519@naver.com>
2023-01-17 19:50:23 +0900

    error : flask server

---
+++ backend/ai/detect.py
@@ -0,0 +1,7 @@
+"""Run inference with a YOLOv5 model on images, videos, directories, streams
+
+Usage:
+    $ python path/to/detect.py --source path/to/img.jpg --weights yolov5s.pt --img 640
+"""
+
+# 여기에 필요한 모델 코드 넣어주시면 됩니다.
