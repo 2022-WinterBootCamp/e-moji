8b0409953e850939d4d9627a112e3f6e341afb40
Ray Lee <wooo519@naver.com>
2023-01-18 15:48:47 +0900

    feat : result model setting

--- backend/backend/faces/utils.py
+++ backend/backend/faces/utils.py
@@ -1,6 +1,7 @@
 import boto3 ,os
 from backend.settings import AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
 from .models import Face
+from emojis.models import Emojis
 
 from uuid import uuid4
 
@@ -20,4 +21,7 @@ def get_img_url(img):
     return image_url
 
 def create_img(user_id, image):
-    return Face.objects.create(user_id = user_id, image = image)
+    return Faces.objects.create(user_id = user_id, image = image)
+
+def create_result(user_id, face_id, emoji_id, kind, image):
+    return Emojis.objects.create(user_id = user_id, face_id = face_id, emoji_id = emoji_id, kind = kind, image = image)
