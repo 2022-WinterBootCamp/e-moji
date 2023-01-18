60dd69d44bbb9a8b77605a13a7ce49e63505a403
Ray Lee <wooo519@naver.com>
2023-01-18 15:48:47 +0900

    feat : result model setting

--- backend/backend/faces/admin.py
+++ backend/backend/faces/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from .models import Faces
+from .models import Result
 
 @admin.register(Faces)
 class FaceAdmin(admin.ModelAdmin):
 
     list_display = ['id','user_id','image']
+
+@admin.register(Result)
+class ResultAdmin(admin.ModelAdmin):
+
+    list_display = ['id','user_id','face_id','emoji_id','kind','image']
--- backend/backend/faces/models.py
+++ backend/backend/faces/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from users.models import Users
+from emojis.models import Emojis
 
 class Faces(models.Model):
     id = models.AutoField(primary_key=True)
@@ -10,3 +11,16 @@ class Faces(models.Model):
 
     class Meta:
         db_table = 'faces'
+
+class Result(models.Model):
+    id = models.AutoField(primary_key=True)
+    user_id = models.ForeignKey(Users, on_delete=models.CASCADE, db_column='user_id')
+    face_id = models.ForeignKey(Faces, on_delete=models.CASCADE, db_column='face_id')
+    emoji_id = models.ForeignKey(Emojis, on_delete=models.CASCADE, db_column='emoji_id')
+    kind = models.IntegerField()
+    image = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True, blank=True)
+    updated_at = models.DateTimeField(auto_now=True, blank=True)
+
+    class Meta:
+        db_table = 'results'
