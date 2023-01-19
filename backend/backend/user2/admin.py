449144d66a3964dc825a95f7673edde62855ec65
daehee <rlaclfjqj1@gmail.com>
2023-01-19 15:09:06 +0900

    feat : token

--- backend/backend/user2/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Users
-# Register your models here.
-
-@admin.register(Users)
-class UserAdmin(admin.ModelAdmin):
-
-    list_display = ['id', 'email', 'password', 'alias', 'active']
--- backend/backend/users/urls.py
+++ backend/backend/users/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from . import views
+from .views import MyTokenObtainPairView
+from rest_framework_simplejwt.views import TokenRefreshView
 
 # user/urls.py
 urlpatterns = [
     path('', views.user),
-    path('test',views.user_test)
-
+    path('test',views.user_test),
+    path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     
 ]
