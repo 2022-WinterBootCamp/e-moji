9a18f5a6ec6b05065e85a863ca1d0fa1a7c9d3ec
Ray Lee <wooo519@naver.com>
2023-01-20 14:57:02 +0900

    feat : token test finish

--- backend/backend/faces/views.py
+++ backend/backend/faces/views.py
@@ -12,21 +12,24 @@ from rest_framework.decorators import api_view
 
 from .serializers import PictureSerializer
 from .utils import get_img_url, create_img
+from users.utils import user_token_to_data
 from users.models import User
 
 @api_view(['POST'])
 def faces(request):
     user_id = request.data['user_id']
     image = request.data['image']
- 
-    userId = User.objects.get(id = user_id)
-    if userId :
+
+    userID= User.objects.get(id = user_id)
+    userData = userID.alias
+    payload = user_token_to_data(request.headers.get('Authorization', None))
+    if (payload.get('alias') == str(userData)):
         # img_url = get_img_url(image)
         img_url = "aaaa"
     else :
         return JsonResponse({"message": "Invalid_User"}, status=401)
 
-    images = create_img(userId, img_url)
+    images = create_img(userID, img_url)
     data = PictureSerializer(images, many=False).data
 
     url = 'http://ai_server:8000/api/v1/images/'
