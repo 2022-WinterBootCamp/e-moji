f0b91db4523858c5e750385e6061e0b53aeaf253
yerin jung <junsesu0531@sungkyul.ac.kr>
2023-01-21 04:48:37 +0900

    fix: firstPage_emoji

--- frontend/src/component/FirstPage.jsx
+++ frontend/src/component/FirstPage.jsx
@@ -5,7 +5,7 @@ import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import { createTheme, ThemeProvider} from '@mui/material/styles';
 import Typography from '@mui/material/Typography';
-import back from './img/back.1.png';
+import back from './img/3.png';
 
 export default function FirstPage() {
 const theme = createTheme({
@@ -30,43 +30,41 @@ const theme = createTheme({
 
   return(
   <Container fixed>
-     <Box> <img src={back} /></Box>
-
-      <Box sx={{
+     <Box sx={{
          width: '100%',
-         height: '100%',
-         position: 'absolute',
-         left: 0,
+         height: '115%',
+         position: 'center',
          backgroundColor: '#FECD93',
-       }}>
-        </Box>
+       }}> <img src={back} /></Box>
+
        
       <Grid container>
        <Box sx={{
         bgcolor: 'white', height: '50vh',
         position: 'absolute',
-        top: '25%',
-        right: '30%',
-        left: '30%',
-        borderRadius: '45px',
+        top: '23%',
+        right: '28.5%',
+        left: '20%',
+        borderRadius: '70px',
       }}
     > 
+    
    <ThemeProvider theme={theme}>
-      <Button variant= "contained" color="inherit" size="large" sx={{top: "40%", marginLeft: "25%", color:'white',
+      <Button variant= "contained" color="inherit" size="large" sx={{top: "40%", marginLeft: "35%", color:'white',
        right: '30%',
        left: '30%', 
        bgcolor: '#FECD93'}} href="/LoginPage" >Click me!</Button>
     <Grid container>
     <Grid item xs={2}>
-      <Typography variant="subtitle1" sx={{top:"15%", marginLeft: "-20%", height: '50vh',
+      <Typography variant="subtitle1" sx={{top:"15%", marginLeft: "-10%", height: '50vh',
         position: 'absolute',
         right: '30%',
         left: '30%',}}>IGE <br/>MOJI</Typography>
       </Grid>
       <Grid item xs={10}>
-        <Typography sx={{mt:"35%", marginLeft: "30%",
+        <Typography sx={{mt:"30%", marginLeft: "45%",
         right: '30%',
-        left: '30%',}}>지금 당신의 기분이 어떠신가요?<br/>
+        left: '40%',}}>지금 당신의 기분이 어떠신가요?<br/>
        거울 속 당신의 표정을 이모지로 만들어 드립니다.</Typography>
        </Grid>
        </Grid>
