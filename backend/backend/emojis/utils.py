07e4ee41e46d596cd7e7f723a8c1a4d05854df76
SOYOON PARK <spurt@tukorea.ac.kr>
2023-01-24 16:21:24 +0900

    fix : profile
    kind delete

--- backend/backend/emojis/utils.py
+++ backend/backend/emojis/utils.py
@@ -7,5 +7,5 @@ from uuid import uuid4
 def create_img(user_id, image):
     return Emoji.objects.create(user_id = user_id, image = image)
 
-def create_emoji(user_id, name, kind, image):
-    return Emoji.objects.create(user_id = user_id, name = name, kind = kind, image = image)
+def create_emoji(user_id, name, image): # kind 삭제
+    return Emoji.objects.create(user_id = user_id, name = name, image = image) # kind 삭제
