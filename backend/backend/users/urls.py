5490d3a01fe28089efefc35e8826b2c97176f9bd
Ray Lee <wooo519@naver.com>
2023-01-26 23:33:53 +0900

    fix : change url

--- backend/backend/users/urls.py
+++ backend/backend/users/urls.py
@@ -5,10 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.user),
     path('test',views.user_test),
-    path('signup', views.sign_up),
     path('auth',views.login),
-    path('check', views.User_duplicate_check),
-    
-         
-        
 ]
