e35fded2c7058561eb061e1810a8eabd1dc85b1f
jincho <chojinieyes@gmail.com>
2023-01-26 22:21:20 +0900

    feat : multi emotion detection

--- backend/ai/detect.py
+++ backend/ai/detect.py
@@ -5,3 +5,16 @@ Usage:
 """
 
 # 여기에 필요한 모델 코드 넣어주시면 됩니다.
+
+from deepface import DeepFace
+from fer import FER
+import cv2
+
+def detect(img):
+    
+    emotion_detector = FER(mtcnn=True)
+    test_img = cv2.imread(img)
+    analysis = emotion_detector.detect_emotions(test_img)
+
+    return analysis
+
