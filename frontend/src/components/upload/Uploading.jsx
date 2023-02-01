ee95541c3e5f62f57e1c54b7823b4771eb73da90
Ray Lee <wooo519@naver.com>
2023-02-01 23:13:43 +0900

    fix : upload error

--- frontend/src/components/upload/Uploading.jsx
+++ frontend/src/components/upload/Uploading.jsx
@@ -1,47 +1,40 @@
-import React, {useEffect, useState} from "react";
-import { 
-  Box, 
-  Container, 
-  Typography 
-} from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { Box, Container, Typography, useStepContext } from "@mui/material";
 import Lottie from "lottie-react";
 import animationData from "../../lotties/emoticon.json";
 import UploadResult from "./UploadResult";
 
-
-export default function Uploading({taskId, aiState}) {
+export default function Uploading({ taskId, aiState }) {
   const [emojiResult, setEmojiResult] = useState("");
   const [aiState_result, setAiState_result] = useState(aiState);
-
-  const polling = setInterval(getResultData(), 3000)
+  let polling = 0;
 
   function place() {
-    console.log("[MainPage - Uploading] 로딩중입니다.")
+    console.log("[MainPage - Uploading] 로딩중입니다.");
     console.log("taskId>>> ", taskId);
   }
 
   //사진 결과 Api
-  async function getResultData(e) {
-    e.preventDefault();
+  async function getResultData() {
+    if (polling === "Done") {
+      return window.clearInterval(polling);
+    }
     try {
-      const task_id = taskId
-      console.log("task_id>>> ", task_id)
-      fetch(
-        `http://localhost:8080/api/v1/faces/results/tasks/${task_id}`,
-        {
-          method: "GET",
-        }
-      )
+      const task_id = taskId;
+      console.log("task_id>>> ", task_id);
+      fetch(`http://localhost:8080/api/v1/faces/results/tasks/${task_id}`, {
+        method: "GET",
+      })
         .then((response) => {
           // console.log(response);
           return response.json();
         })
         .then((data) => {
-          if(data.ai_result !== 'notyet'){
-            setAiState_result(2)
+          if (data.ai_result !== "notyet") {
+            setAiState_result(2);
             setEmojiResult(data);
             console.log("[getResultData]data>>> ", data);
-            return clearInterval(polling);
+            polling = "Done";
           }
           console.log("[getResultData-로딩중]data>>> ", data);
         });
@@ -49,6 +42,13 @@ export default function Uploading({taskId, aiState}) {
       console.log(err);
     }
   }
+
+  useEffect(() => {
+    const polling = window.setInterval(() => getResultData(), 3000);
+    return () => {
+      window.clearInterval(polling);
+    };
+  }, [taskId]);
 
   return (
     <Box
@@ -61,38 +61,35 @@ export default function Uploading({taskId, aiState}) {
       }}
     >
       {place()}
-      {
-        aiState_result === 1
-        ? <Container maxWidth="sm">
-            <Typography
-              component="h1"
-              variant="h4"
-              align="center"
-              color="text.primary"
-              gutterBottom
-              fontStyle="bold"
-              fontFamily="Itim"
-            >
-              AI가 당신의 표정을 분석 중이에요!
-            </Typography>
-            <Typography
-              variant="h6"
-              align="center"
-              color="text.secondary"
-              paragraph
-            >
-              잠시만 기다려주세요...
-            </Typography>
-            <Lottie
-              animationData={animationData}
-              style={{ height: "350px", marginTop: -25 }}
-            />
-          </Container>
-        : (aiState_result === 2)
-          ? <UploadResult emojiResult={emojiResult}/>
-          : null
-      }
-      
+      {aiState_result === 1 ? (
+        <Container maxWidth="sm">
+          <Typography
+            component="h1"
+            variant="h4"
+            align="center"
+            color="text.primary"
+            gutterBottom
+            fontStyle="bold"
+            fontFamily="Itim"
+          >
+            AI가 당신의 표정을 분석 중이에요!
+          </Typography>
+          <Typography
+            variant="h6"
+            align="center"
+            color="text.secondary"
+            paragraph
+          >
+            잠시만 기다려주세요...
+          </Typography>
+          <Lottie
+            animationData={animationData}
+            style={{ height: "350px", marginTop: -25 }}
+          />
+        </Container>
+      ) : aiState_result === 2 ? (
+        <UploadResult emojiResult={emojiResult} />
+      ) : null}
     </Box>
   );
 }
